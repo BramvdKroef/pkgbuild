bc86a799f1063dbca5b16d94791dd8ab146d146f
Bram van der Kroef <bram@vanderkroef.net>
2017-12-19 14:22:50 -0600

    Adding custom dwmstatus.

---
+++ dwmstatus/config.h
@@ -0,0 +1,18 @@
+
+struct mailmonitor_data maildata = {
+  "/home/bram/mail/INBOX/new",
+  0};
+
+struct weather_data weatherdata = {
+  "https://weather.gc.ca/rss/city/on-159_e.xml",
+  "/atom:feed/atom:entry/atom:category[@term=\"Current Conditions\"]/../atom:title/text()",
+  "/atom:feed/atom:entry/atom:category[@term=\"Warnings and Watches\"]/../atom:title/text()"
+};
+
+CtlData ctl_p[4] = {
+  {8, "%s", weather_ctl, NULL, &weatherdata},
+  {9, "RAM: % 3.0f%%", ram_ctl, NULL, NULL},
+  {9, "Email: %02d", mailmonitor_ctl, NULL, &maildata},
+  {23, "%e %b %Y %a | %k:%M", datetime_ctl, NULL, NULL}
+};
+size_t ctl_n = 4;
