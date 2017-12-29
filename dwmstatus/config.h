46c684fabf435ba98e80152f41e0532a87edfe6d
Bram van der Kroef <bram@vanderkroef.net>
2017-12-29 09:45:28 -0600

    Change email format in config.

--- dwmstatus/config.h
+++ dwmstatus/config.h
@@ -12,7 +12,7 @@ struct weather_data weatherdata = {
 CtlData ctl_p[4] = {
   {8, "%s", weather_ctl, NULL, &weatherdata},
   {9, "RAM: % 3.0f%%", ram_ctl, NULL, NULL},
-  {9, "Email: %02d", mailmonitor_ctl, NULL, &maildata},
+  {9, "Email: %2d", mailmonitor_ctl, NULL, &maildata},
   {23, "%e %b %Y %a | %k:%M", datetime_ctl, NULL, NULL}
 };
 size_t ctl_n = 4;
